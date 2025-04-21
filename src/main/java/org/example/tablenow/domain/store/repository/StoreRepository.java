0415ae989614a9f5628c132714c5a0f09f28fe9b
yrchoi <daylikezero@gmail.com>
2025-04-21 14:24:01 +0900

    feat(search): ElasticSearch 인덱스 Bulk Insert #137

--- src/main/java/org/example/tablenow/domain/store/repository/StoreRepository.java
+++ src/main/java/org/example/tablenow/domain/store/repository/StoreRepository.java
@@ -3,10 +3,16 @@ package org.example.tablenow.domain.store.repository;
 import org.example.tablenow.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long>, StoreQueryRepository {
     @EntityGraph(attributePaths = {"category"}, type = EntityGraph.EntityGraphType.FETCH)
     Optional<Store> findByIdAndDeletedAtIsNull(Long id);
+
+    @EntityGraph(attributePaths = {"user", "category"})
+    @Query("SELECT s FROM Store s")
+    List<Store> findAllWithUserAndCategory();
 }
