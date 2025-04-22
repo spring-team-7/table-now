d8e21a381417797d8ee34a9110dccfd84ef746a3
yrchoi <daylikezero@gmail.com>
2025-04-22 15:21:42 +0900

    refactor(search): 가게 검색 공통 반환타입 적용 #156

--- src/main/java/org/example/tablenow/domain/store/service/StoreSearchService.java
+++ src/main/java/org/example/tablenow/domain/store/service/StoreSearchService.java
@@ -6,7 +6,6 @@ import org.example.tablenow.domain.store.entity.StoreDocument;
 import org.example.tablenow.domain.store.enums.StoreSortField;
 import org.example.tablenow.domain.store.repository.StoreElasticRepository;
 import org.example.tablenow.domain.store.util.StoreKeyGenerator;
-import org.example.tablenow.global.dto.PageResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,24 +22,25 @@ public class StoreSearchService {
 
     private final StoreElasticRepository storeElasticRepository;
     private final RedisTemplate<Object, Object> redisTemplate;
+    private static final String SEARCH_KEY_PREFIX = "store:search:";
 
     @Transactional(readOnly = true)
-    public PageResponse<StoreDocumentResponseDto> getStoresV3(int page, int size, String sort, String direction, Long categoryId, String keyword) {
+    public Page<StoreDocumentResponseDto> getStoresV3(int page, int size, String sort, String direction, Long categoryId, String keyword) {
         Sort sortOption = Sort.by(Sort.Direction.fromString(direction), StoreSortField.fromString(sort));
         Pageable pageable = PageRequest.of(page - 1, size, sortOption);
 
         // Redis 조회
-        String storeKey = StoreKeyGenerator.generateStoreListKey(page, size, sort, direction, categoryId, keyword);
+        String storeKey = SEARCH_KEY_PREFIX + StoreKeyGenerator.generateStoreListKey(page, size, sort, direction, categoryId, keyword);
         if (redisTemplate.hasKey(storeKey)) {
             Object storeCache = redisTemplate.opsForValue().get(storeKey);
-            if (storeCache instanceof PageResponse<?>) {
-                return (PageResponse<StoreDocumentResponseDto>) storeCache;
+            if (storeCache instanceof Page<?>) {
+                return (Page<StoreDocumentResponseDto>) storeCache;
             }
         }
 
         // ElasticSearch 조회
         Page<StoreDocument> storeDocuments = storeElasticRepository.searchByKeywordAndCategoryId(keyword, categoryId, pageable);
-        PageResponse<StoreDocumentResponseDto> response = new PageResponse<>(storeDocuments.map(storeDocument -> StoreDocumentResponseDto.fromStoreDocument(storeDocument)));
+        Page<StoreDocumentResponseDto> response = storeDocuments.map(storeDocument -> StoreDocumentResponseDto.fromStoreDocument(storeDocument));
 
         if (!response.getContent().isEmpty()) {
             // Redis 캐시 저장
