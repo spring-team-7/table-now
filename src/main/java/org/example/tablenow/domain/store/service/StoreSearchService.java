eafd69c181101d61669d6ec9e069aaf75429b572
yrchoi <daylikezero@gmail.com>
2025-04-22 17:34:17 +0900

    refactor(search): 가게 서비스 테스트코드 리팩토링 #156

--- src/main/java/org/example/tablenow/domain/store/service/StoreSearchService.java
+++ src/main/java/org/example/tablenow/domain/store/service/StoreSearchService.java
@@ -6,10 +6,10 @@ import org.example.tablenow.domain.store.entity.StoreDocument;
 import org.example.tablenow.domain.store.enums.StoreSortField;
 import org.example.tablenow.domain.store.repository.StoreElasticRepository;
 import org.example.tablenow.domain.store.util.StoreKeyGenerator;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.example.tablenow.global.dto.AuthUser;
+import org.example.tablenow.global.exception.ErrorCode;
+import org.example.tablenow.global.exception.HandledException;
+import org.springframework.data.domain.*;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,32 +21,38 @@ import java.util.concurrent.TimeUnit;
 public class StoreSearchService {
 
     private final StoreElasticRepository storeElasticRepository;
+    private final StoreService storeService;
     private final RedisTemplate<Object, Object> redisTemplate;
     private static final String SEARCH_KEY_PREFIX = "store:search:";
 
     @Transactional(readOnly = true)
-    public Page<StoreDocumentResponseDto> getStoresV3(int page, int size, String sort, String direction, Long categoryId, String keyword) {
-        Sort sortOption = Sort.by(Sort.Direction.fromString(direction), StoreSortField.fromString(sort));
-        Pageable pageable = PageRequest.of(page - 1, size, sortOption);
+    public Page<StoreDocumentResponseDto> getStoresV3(AuthUser authUser, int page, int size, String sort, String direction, Long categoryId, String keyword) {
+        try {
+            Sort sortOption = Sort.by(Sort.Direction.fromString(direction), StoreSortField.fromString(sort));
+            Pageable pageable = PageRequest.of(page - 1, size, sortOption);
 
-        // Redis 조회
-        String storeKey = SEARCH_KEY_PREFIX + StoreKeyGenerator.generateStoreListKey(page, size, sort, direction, categoryId, keyword);
-        if (redisTemplate.hasKey(storeKey)) {
-            Object storeCache = redisTemplate.opsForValue().get(storeKey);
-            if (storeCache instanceof Page<?>) {
+            // 인기 검색어 저장
+            storeService.savePopularKeyword(authUser, keyword);
+
+            // Redis 조회
+            String storeKey = SEARCH_KEY_PREFIX + StoreKeyGenerator.generateStoreListKey(page, size, sort, direction, categoryId, keyword);
+            if (redisTemplate.hasKey(storeKey)) {
+                Object storeCache = redisTemplate.opsForValue().get(storeKey);
                 return (Page<StoreDocumentResponseDto>) storeCache;
             }
-        }
 
-        // ElasticSearch 조회
-        Page<StoreDocument> storeDocuments = storeElasticRepository.searchByKeywordAndCategoryId(keyword, categoryId, pageable);
-        Page<StoreDocumentResponseDto> response = storeDocuments.map(storeDocument -> StoreDocumentResponseDto.fromStoreDocument(storeDocument));
+            // ElasticSearch 조회
+            Page<StoreDocument> storeDocuments = storeElasticRepository.searchByKeywordAndCategoryId(keyword, categoryId, pageable);
+            Page<StoreDocumentResponseDto> response = storeDocuments.map(storeDocument -> StoreDocumentResponseDto.fromStoreDocument(storeDocument));
 
-        if (!response.getContent().isEmpty()) {
-            // Redis 캐시 저장
-            redisTemplate.opsForValue().set(storeKey, response);
-            redisTemplate.expire(storeKey, 1, TimeUnit.DAYS);
+            if (!response.getContent().isEmpty()) {
+                // Redis 캐시 저장
+                redisTemplate.opsForValue().set(storeKey, response);
+                redisTemplate.expire(storeKey, 1, TimeUnit.DAYS);
+            }
+            return response;
+        } catch (IllegalArgumentException e) {
+            throw new HandledException(ErrorCode.INVALID_ORDER_VALUE);
         }
-        return response;
     }
 }
--- src/test/java/org/example/tablenow/domain/store/performance/StorePerformanceTest.java
+++ src/test/java/org/example/tablenow/domain/store/performance/StorePerformanceTest.java
@@ -1,9 +1,9 @@
 package org.example.tablenow.domain.store.performance;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.tablenow.config.WithMockAuthUser;
+import org.example.tablenow.domain.store.repository.StoreElasticRepository;
 import org.example.tablenow.domain.store.repository.StoreRepository;
-import org.example.tablenow.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -35,58 +35,144 @@ public class StorePerformanceTest {
     @MockitoSpyBean
     StoreRepository storeRepository;
 
-    @Test
-    @WithMockAuthUser(userId = 1L, email = "test@test.com", role = UserRole.ROLE_USER, nickname = "테스트")
-    void 가게_목록_조회_캐시_성능_테스트() throws Exception {
-        int iterations = 1000;
-        long totalDuration = 0L;
+    @MockitoSpyBean
+    StoreElasticRepository storeElasticRepository;
 
-        for (int i = 0; i < iterations; i++) {
-            Instant start = Instant.now();
-            mockMvc.perform(get("/api/v1/stores")
-                    .param("page", "1")
-                    .param("size", "10")
-                    .param("sort", "rating")
-                    .param("direction", "desc")
-                    .param("categoryId", "1")
-                    .param("search", "가게")
-            ).andExpect(status().isOk());
-            totalDuration += Duration.between(start, Instant.now()).toMillis();
+    @Nested
+    class 가게_검색_v1 {
+        @Test
+        void 가게_목록_DB_조회() throws Exception {
+            int iterations = 100;
+            long totalDuration = 0L;
+
+            for (int i = 0; i < iterations; i++) {
+                Instant start = Instant.now();
+                mockMvc.perform(get("/api/v1/stores")
+                        .param("page", "1")
+                        .param("size", "10")
+                        .param("sort", "rating")
+                        .param("direction", "desc")
+                        .param("categoryId", "1")
+                        .param("search", "테스트")
+                ).andExpect(status().isOk());
+                totalDuration += Duration.between(start, Instant.now()).toMillis();
+            }
+            log.info("평균 응답 시간: {}ms", (totalDuration / (double) iterations));
         }
-        log.info("평균 응답 시간: {}ms", (totalDuration / (double) iterations));
     }
 
-    @Test
-    void 동시_요청_시_redis_캐시_조회_검증() throws InterruptedException {
-        int threadCount = 200;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+    @Nested
+    class 가게_검색_v2 {
+        @Test
+        void 가게_목록_redis_캐시_조회() throws Exception {
+            int iterations = 100;
+            long totalDuration = 0L;
 
-        for (int i = 0; i < threadCount; i++) {
-            executorService.submit(() -> {
-                try {
-                    mockMvc.perform(get("/api/v1/stores")
-                            .param("page", "1")
-                            .param("size", "10")
-                            .param("sort", "rating")
-                            .param("direction", "desc")
-                            .param("categoryId", "1")
-                            .param("search", "가게")
-                    ).andExpect(status().isOk());
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                } finally {
-                    countDownLatch.countDown();
-                }
-            });
+            for (int i = 0; i < iterations; i++) {
+                Instant start = Instant.now();
+                mockMvc.perform(get("/api/v2/stores")
+                        .param("page", "1")
+                        .param("size", "10")
+                        .param("sort", "rating")
+                        .param("direction", "desc")
+                        .param("categoryId", "1")
+                        .param("search", "테스트")
+                ).andExpect(status().isOk());
+                totalDuration += Duration.between(start, Instant.now()).toMillis();
+            }
+            log.info("평균 응답 시간: {}ms", (totalDuration / (double) iterations));
         }
-        countDownLatch.await();
 
-        verify(storeRepository, atMost(1))
-                .searchStores(
-                        any(Pageable.class),
-                        anyLong(),
-                        anyString()
-                );
+        @Test
+        void 동시_요청_시_redis_캐시_조회_검증() throws InterruptedException {
+            int threadCount = 200;
+            ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+            CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+            for (int i = 0; i < threadCount; i++) {
+                executorService.submit(() -> {
+                    try {
+                        mockMvc.perform(get("/api/v2/stores")
+                                .param("page", "1")
+                                .param("size", "10")
+                                .param("sort", "rating")
+                                .param("direction", "desc")
+                                .param("categoryId", "1")
+                                .param("search", "테스트")
+                        ).andExpect(status().isOk());
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                });
+            }
+            countDownLatch.await();
+
+            verify(storeRepository, atMost(1))
+                    .searchStores(
+                            any(Pageable.class),
+                            anyLong(),
+                            anyString()
+                    );
+        }
     }
+
+    @Nested
+    class 가게_검색_v3 {
+        @Test
+        void 가게_목록_ES_조회() throws Exception {
+            int iterations = 100;
+            long totalDuration = 0L;
+
+            for (int i = 0; i < iterations; i++) {
+                Instant start = Instant.now();
+                mockMvc.perform(get("/api/v3/stores")
+                        .param("page", "1")
+                        .param("size", "10")
+                        .param("sort", "rating")
+                        .param("direction", "desc")
+                        .param("categoryId", "1")
+                        .param("search", "테스트")
+                ).andExpect(status().isOk());
+                totalDuration += Duration.between(start, Instant.now()).toMillis();
+            }
+            log.info("평균 응답 시간: {}ms", (totalDuration / (double) iterations));
+        }
+
+        @Test
+        void 동시_요청_시_redis_캐시_조회_검증() throws InterruptedException {
+            int threadCount = 200;
+            ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+            CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+            for (int i = 0; i < threadCount; i++) {
+                executorService.submit(() -> {
+                    try {
+                        mockMvc.perform(get("/api/v3/stores")
+                                .param("page", "1")
+                                .param("size", "10")
+                                .param("sort", "rating")
+                                .param("direction", "desc")
+                                .param("categoryId", "1")
+                                .param("search", "테스트")
+                        ).andExpect(status().isOk());
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                });
+            }
+            countDownLatch.await();
+
+            verify(storeElasticRepository, atMost(1))
+                    .searchByKeywordAndCategoryId(
+                            anyString(),
+                            anyLong(),
+                            any(Pageable.class)
+                    );
+        }
+    }
+
 }
