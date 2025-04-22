e0f961866a0aff3dbc3f5779e5f3b8b1c1371fb7
3uomlkh <3uomlkh@gmail.com>
2025-04-22 20:25:48 +0900

    feat(event) : 이벤트 오픈 fanout exchange 및 큐 바인딩 설정 추가 #157
    ### 변경점
    
    - event.open.notification.queue 등록
    - fanout exchange(event.open.fanout) 등록
    - 큐와 exchange 바인딩 설정
    - RabbitConstant에 이벤트 오픈 관련 상수 추가

--- src/main/java/org/example/tablenow/global/rabbitmq/constant/RabbitConstant.java
+++ src/main/java/org/example/tablenow/global/rabbitmq/constant/RabbitConstant.java
@@ -4,4 +4,7 @@ public class RabbitConstant {
     public static final String VACANCY_EXCHANGE = "vacancy.direct";
     public static final String VACANCY_QUEUE = "vacancy.queue";
     public static final String VACANCY_ROUTING_KEY = "vacancy.key";
+
+    public static final String EVENT_OPEN_EXCHANGE = "event.open.fanout";
+    public static final String EVENT_OPEN_QUEUE = "event.open.notification.queue";
 }
