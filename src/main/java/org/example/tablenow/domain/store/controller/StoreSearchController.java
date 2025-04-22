3c3073627e2651030938ab4773612a379ae5bdcb
yrchoi <daylikezero@gmail.com>
2025-04-22 15:21:42 +0900

    refactor(search): 가게 검색 공통 반환타입 적용 #156

--- src/main/java/org/example/tablenow/domain/store/controller/StoreSearchController.java
+++ src/main/java/org/example/tablenow/domain/store/controller/StoreSearchController.java
@@ -3,7 +3,7 @@ package org.example.tablenow.domain.store.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.tablenow.domain.store.dto.response.StoreDocumentResponseDto;
 import org.example.tablenow.domain.store.service.StoreSearchService;
-import org.example.tablenow.global.dto.PageResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +15,12 @@ public class StoreSearchController {
     private final StoreSearchService storeSearchService;
 
     @GetMapping("/v3/stores")
-    public ResponseEntity<PageResponse<StoreDocumentResponseDto>> getStoresV3(@RequestParam(defaultValue = "1") int page,
-                                                                              @RequestParam(defaultValue = "10") int size,
-                                                                              @RequestParam(defaultValue = "ratingCount") String sort,
-                                                                              @RequestParam(defaultValue = "desc") String direction,
-                                                                              @RequestParam(required = false) Long categoryId,
-                                                                              @RequestParam(required = false) String keyword) {
+    public ResponseEntity<Page<StoreDocumentResponseDto>> getStoresV3(@RequestParam(defaultValue = "1") int page,
+                                                                      @RequestParam(defaultValue = "10") int size,
+                                                                      @RequestParam(defaultValue = "ratingCount") String sort,
+                                                                      @RequestParam(defaultValue = "desc") String direction,
+                                                                      @RequestParam(required = false) Long categoryId,
+                                                                      @RequestParam(required = false) String keyword) {
         return ResponseEntity.ok(storeSearchService.getStoresV3(page, size, sort, direction, categoryId, keyword));
     }
 }
