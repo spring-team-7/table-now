e13462bb05370deb3405e8099917112016fc0d1c
3uomlkh <3uomlkh@gmail.com>
2025-04-23 10:57:31 +0900

    feat(user) : 알람 설정된 유저 조회 기능 추가 #157
    ### 변경점
    
    - 알람이 활성화된 유저만 조회하는 getUsersWithAlarmEnabled() 메서드 추가
    - UserRepository에 findAllByIsAlarmEnabledTrue() 메서드 추가

--- src/main/java/org/example/tablenow/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/tablenow/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package org.example.tablenow.domain.user.repository;
 import org.example.tablenow.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    List<User> findAllByIsAlarmEnabledTrue();
 }
