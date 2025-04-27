64d0934bfbab9ba91459c9e00f2ebcdfecf19b98
3uomlkh <3uomlkh@gmail.com>
2025-04-27 17:25:54 +0900

    refactor(reservation): 요청 DTO의 @Builder 생성자 제거 #198
    ### 변경점
    
    - 요청 DTO의 @Builder 생성자 제거
    - @AllArgsConstructor 추가

--- src/main/java/org/example/tablenow/domain/reservation/dto/request/ReservationStatusChangeRequestDto.java
+++ src/main/java/org/example/tablenow/domain/reservation/dto/request/ReservationStatusChangeRequestDto.java
@@ -1,19 +1,15 @@
 package org.example.tablenow.domain.reservation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.tablenow.domain.reservation.entity.ReservationStatus;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReservationStatusChangeRequestDto {
     @NotNull
     private ReservationStatus status;
-
-    @Builder
-    public ReservationStatusChangeRequestDto(ReservationStatus status) {
-        this.status = status;
-    }
 }
