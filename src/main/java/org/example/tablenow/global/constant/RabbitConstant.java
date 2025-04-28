e5e72761f2e1829b7035accbe7e3db08c30ab672
3uomlkh <3uomlkh@gmail.com>
2025-04-28 16:05:54 +0900

    refactor(reservation): Producer에서 라우팅키 삭제 #203
    ### 변경점
    
    - 라우팅키, 라우팅키 상수 삭제

--- src/main/java/org/example/tablenow/global/constant/RabbitConstant.java
+++ src/main/java/org/example/tablenow/global/constant/RabbitConstant.java
@@ -13,11 +13,9 @@ public class RabbitConstant {
 
     public static final String RESERVATION_REMINDER_REGISTER_EXCHANGE = "reservation.reminder.register.exchange";
     public static final String RESERVATION_REMINDER_REGISTER_QUEUE = "reservation.reminder.register.queue";
-    public static final String RESERVATION_REMINDER_REGISTER_ROUTING_KEY = "reservation.reminder.register.key";
 
     public static final String RESERVATION_REMINDER_SEND_EXCHANGE = "reservation.reminder.send.exchange";
     public static final String RESERVATION_REMINDER_SEND_QUEUE = "reservation.reminder.send.queue";
-    public static final String RESERVATION_REMINDER_SEND_ROUTING_KEY = "reservation.reminder.send.key";
 
     public static final String STORE_EXCHANGE = "store.exchange";
     public static final String STORE_CREATE = "store.create";
